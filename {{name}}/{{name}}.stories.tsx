50a9231d28d07bcc861968476c27475f9789a03e
Andrew Brough <spectrum1012@gmail.com>
2020-03-09 00:23:50 -0400

    deleted story

--- {{name}}/{{name}}.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import { storiesOf } from "@storybook/react";
-
-import { {{name}} } from ".";
-
-storiesOf("{{name}}", module).add("default", () => {
-  return <{{name}} />;
-});
